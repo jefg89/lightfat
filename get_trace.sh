7a5777d76c56613c2061a09595e3c701394082ea
jgonzalez <jgonzalez@itcr.ac.cr>
2022-10-20 16:18:51 +0200

    bash traced updated

--- get_trace.sh
+++ get_trace.sh
@@ -7,9 +7,10 @@ pid=`pidof $1`
 sampling_period=10
 
 # call perf to get samples
-sudo perf stat -I $sampling_period -e cycles,instructions,cache-references -p $pid  -o tmp.log sleep 1
+sudo perf stat -B -I $sampling_period -e cycles,instructions,L1-dcache-loads  -p $pid  -o tmp.log  sleep 1
 
 #create sub-files
+cat tmp.log | grep "cache" > cache_raw.log; cat cache_raw.log | grep -v not > cache.out
+cat tmp.log | grep "ins" > ipc_raw.log; cat ipc_raw.log | grep -v not > ipc.out
+rm -rf *.log
 
-cat tmp.log | grep "cache" > cache.out
-cat tmp.log | grep "ins" > ipc.out
