3b8db6d4a7da24a1319fc9e760b54a2db47916c7
jgonzalez <jgonzalez@itcr.ac.cr>
2022-12-13 10:59:22 +0100

    Fix: reduce default sample size on pcf2bdf monitor

--- monitor_pcf2bdf.sh
+++ monitor_pcf2bdf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 app=pcf2bdf/pcf2bdf
 
-for x in {0..200}
+for x in {0..23}
 do
     for file in data/fonts/*
-    do 
+    do
         scripts/get_trace_of_arg.sh $app $file
         sleep 1.5
     done
